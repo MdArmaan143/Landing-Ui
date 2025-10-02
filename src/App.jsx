4156a52040aa73dbf66a41480f0872ce691308c1
Md Armaan <armaanshaikhh786@gmail.com>
2025-10-02 16:24:49 +0530

    done1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import Header from './components/Header'
-import HeroSection from './components/Herosection';
+import HeroSection from './components/HeroSection';
 import Impact from './components/Impact';
 import './App.css';
 
